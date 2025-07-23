67c6238ba67e744e44d31578e02538a1ea7fd9fa
Tomas Vallejos <tomas.vallejos12@gmail.com>
2025-07-23 15:14:03 +0200

    partially generating the type of the injection lemma

--- generic/injection_lemmas.v
+++ generic/injection_lemmas.v
@@ -19,9 +19,67 @@ Elpi Db derive.injections.db lp:{{
   pred injections-done o:gref o:int.
 }}.
 
+Elpi Program test lp:{{ 
+  kind  term  type.
+  type  app   term -> term -> term.
+  type  fun   (term -> term) -> term.
+
+  kind  ty   type.           % the data type of types
+  type  arr  ty -> ty -> ty. % our type constructor
+
+  pred of i:term, o:ty. % the type checking algorithm
+  of (app X Y) B :- 
+    of X (arr A B), 
+    of Y A.
+  of (fun Bo) (arr A B) :-
+  pi x\
+    of x A ==>
+    of (Bo x) B.
+
+}}.
+(* Elpi Query lp:{{
+  coq.locate "list" (indt GR),
+  coq.locate "nat" (indt GR2),
+
+  coq.mk-apps (global (indt GR)) [global (indt GR2)] ListNat,
+
+  coq.say "list nat:" ListNat
+}}. *)
+
+(* Elpi Query lp:{{
+  coq.locate "list" (indt GR),
+  coq.locate "nat" (indt GR2),
+  L is (global (indt GR)),
+  N is (global (indt GR2)),
+  coq.mk-app L [N] ListNat,
+  %coq.say EQ,
+  coq.say ListNat.
+
+}}. *)
+
+Elpi Query lp:{{
+
+  coq.locate "list" (indt GR),
+  coq.locate "nat" (indt GR2),
+  %
+  coq.say R.
+  %F1 = {{ fun (A : Type) => lp:{{global (indt GR)}} A }},
+  %coq.typecheck F1 _ ok.
+
+}}.
+
+(* Elpi Query lp:{{ 
+  
+  F = fun `A` {{ Type }} x\ {coq.mk-app (global (indt GR)) [x]},
+  %F = fun `A` {{ Type }} x\ {{ lp:(global (indt GR)) lp:(x) }}, 
+  coq.typecheck F _ ok.
+ 
+  (* coq.say F. *)
+}}. *)
+
 Elpi Command derive.injections.
 Elpi Accumulate File derive_hook.
-(* Elpi Accumulate Db Header derive.projK.db. *)
+Elpi Accumulate Db Header derive.projK.db.
 Elpi Accumulate Db derive.projK.db.
 Elpi Accumulate File injections.
 Elpi Accumulate Db derive.injections.db.
@@ -38,10 +96,15 @@ Elpi Accumulate lp:{{
   pred usage.
   usage :- coq.error "Usage: derive.injections <object name>".
 }}. 
-
 Elpi Trace Browser.
+
+Elpi derive.projK nat.
+Elpi derive.injections nat.
+Print nat_injections21.
+
 Elpi derive.projK list.
-Elpi derive.injections list.
+Print projcons1.
+Fail Elpi derive.injections list.
 
 Elpi Query lp:{{
 
@@ -49,5 +112,3 @@ Elpi Query lp:{{
   coq.say "Constructor " B ""
 
 }}.
-Elpi derive.projK nat.
-Elpi derive.injections nat.
