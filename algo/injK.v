3e49b9b492e5bc8cdaae520e6b37abd13d1e81bf
Tomas Vallejos <tomas.vallejos12@gmail.com>
2025-08-21 15:57:06 +0200

    refactoring + cleaning

--- algo/injK.v
+++ algo/injK.v
@@ -2,9 +2,11 @@
 From elpi.apps.derive.elpi Extra Dependency "derive_hook.elpi" as derive_hook.
 From Trocq.Algo Extra Dependency "injK.elpi" as injK.
 From Trocq.Algo Extra Dependency "utils.elpi" as algo_utils.
+From Trocq.Algo Require Import injection_lemmas. 
 
 From elpi Require Import elpi.
 From elpi.apps Require Import derive.bcongr. (* for eq_f register *) 
+(* From elpi.apps Require Import projK.  *)
 
 From Trocq Require Import HoTT_additions Hierarchy.
 Unset Uniform Inductive Parameters. 
@@ -32,6 +34,7 @@ Elpi Command derive.injectionsK.
 Elpi Accumulate File derive_hook.
 Elpi Accumulate File algo_utils.
 Elpi Accumulate Db derive.injectionsK.db.
+Elpi Accumulate Db derive.injections.db.
 Elpi Accumulate File injK.
 Elpi Accumulate lp:{{
   main [str I] :- !, coq.locate I (indt GR),
@@ -44,3 +47,7 @@ Elpi Accumulate lp:{{
   usage :- coq.error "Usage: derive.Rm <object name>".
 }}. 
 
+From Trocq.Tests Require Import coverage.
+Elpi derive.projK Box.
+Elpi derive.injections Box.
+Elpi derive.injectionsK Box.
