537d12bda613fe90e3bdccad5bb3108e0f221d7e
Tomas Vallejos <tomas.vallejos12@gmail.com>
2025-09-05 17:24:46 +0200

    mRRmK for inds with multiple constructors + multiple arguments. No recursion No Typaram

--- std/algo/theories/mRRmK.v
+++ std/algo/theories/mRRmK.v
@@ -17,11 +17,11 @@ Elpi Db derive.mRRmK.db lp:{{
   % [ar-db A1 A2 AR] returns the relation between a type A1 and A2.
   pred ar-db i:term, i:term, o:term. 
 
-  % [mRRm-db T D] links a type T to its corresponding R in map.
-  pred mRRm-db i:term, o:term.
+  % [mRRmK-db T D] links a type T to its corresponding R in map.
+  pred mRRmK-db i:term, o:term.
 
-  % [mRRm-done T] mean T was already derived
-  pred rm-done o:term.
+  % [mRRmK-done T] mean T was already derived
+  pred mRRmK-done o:term.
 }}.
 
 Elpi Command derive.mRRmK.
@@ -38,6 +38,7 @@ Elpi Accumulate Db derive.mR.db.
 Elpi Accumulate Db Header derive.Rm.db.
 Elpi Accumulate Db derive.Rm.db.
 
+Elpi Accumulate Db Header derive.mRRmK.db.
 Elpi Accumulate Db derive.mRRmK.db.
 Elpi Accumulate File mRRmK.
 Elpi Accumulate lp:{{
@@ -51,44 +52,3 @@ Elpi Accumulate lp:{{
   usage :- coq.error "Usage: derive.mRRmK <object name>".
 }}. 
 
-(* From Trocq Require Import coverage.
-Elpi derive.param2 Unit.
-Elpi derive.mymap Unit.
-Elpi derive.projK Unit.
-Elpi derive.injections Unit.
-Elpi derive.isK Unit.
-Elpi derive.mR Unit.
-Elpi derive.Rm Unit.
-Elpi derive.injK Unit.
-Elpi derive.mRRmK Unit.
-
-Elpi derive.param2 Bool.
-Elpi derive.mymap Bool.
-Elpi derive.projK Bool.
-Elpi derive.injections Bool.
-Elpi derive.isK Bool.
-Elpi derive.mR Bool.
-Elpi derive.Rm Bool.
-Elpi derive.injK Bool.
-Elpi derive.mRRmK Bool.
-
-Elpi derive.param2 Wrap.
-Elpi derive.mymap Wrap.
-Elpi derive.projK Wrap.
-Elpi derive.injections Wrap.
-Elpi derive.isK Wrap.
-Elpi derive.mR Wrap.
-Elpi derive.Rm Wrap.
-Elpi derive.injK Wrap.
-Elpi Trace Browser.
-Elpi derive.mRRmK Wrap.
-
-Elpi derive.param2 WrapMore.
-Elpi derive.mymap WrapMore.
-Elpi derive.projK WrapMore.
-Elpi derive.injections WrapMore.
-Elpi derive.isK WrapMore.
-Elpi derive.mR WrapMore.
-Elpi derive.Rm WrapMore.
-Elpi derive.injK WrapMore.
-Elpi derive.mRRmK WrapMore. *)
