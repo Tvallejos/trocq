a0ae968a57c1497df4711ac9d47b70883eade79c
Tomas Vallejos <tomas.vallejos12@gmail.com>
2026-01-19 11:18:30 +0100

    swap derivation

---
+++ std/algo/tests/test_swap.v
@@ -0,0 +1,30 @@
+From elpi Require Import elpi.
+From elpi.apps Require Import derive.param2.
+From Trocq Require Import coverage swap.
+
+Elpi derive.param2 False.
+Elpi derive.swap False.
+
+Elpi derive.param2 Unit.
+Elpi derive.swap Unit.
+
+Elpi derive.param2 Bool.
+Elpi derive.swap Bool.
+
+Elpi derive.param2 Wrap.
+Elpi derive.swap Wrap.
+
+Elpi derive.param2 WrapMore.
+Elpi derive.swap WrapMore.
+
+Elpi derive.param2 nat.
+Elpi derive.swap nat.
+
+Elpi derive.param2 option.
+Elpi derive.swap option.
+
+Elpi derive.param2 prod.
+Elpi derive.swap prod.
+
+Elpi derive.param2 list.
+Elpi derive.swap list.
