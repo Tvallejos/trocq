32b377f4661141ad6684e39915471c2165fd0d72
Tomas Vallejos <tomas.vallejos12@gmail.com>
2026-01-20 13:25:38 +0100

    split sym and symK + changing a bit the generated type for sym

---
+++ std/algo/detailed/Detailed_symK.v
@@ -0,0 +1,151 @@
+From Coq Require Import ssreflect.
+From elpi.apps.derive Require Import derive derive.param2.
+Require Import Hierarchy.
+From Trocq Require Import coverage sym.
+(* Require Import HoTT_additions Hierarchy. *)
+Unset Uniform Inductive Parameters.
+(* Unset Universe Polymorphism. *)
+Unset Universe Minimization ToSet.
+
+(* Definition Unit := unit. *)
+
+Elpi derive.param2 Unit.
+Elpi derive.sym Unit.
+
+Definition Unit_symKPred := fun u1 u2 (uR : Unit_R u1 u2)=> Unit_sym _ _ (Unit_sym _ _ uR) = uR.
+Definition Unit_symK : forall u1 u2 (uR : Unit_R u1 u2), Unit_sym _ _ (Unit_sym _ _ uR) = uR.
+Proof.
+move=> u1 u2 uR.
+refine (Unit_R_ind Unit_symKPred _ u1 u2 uR).
+exact: eq_refl.
+Defined.
+
+Elpi derive.param2 Bool.
+Elpi derive.sym Bool.
+
+Notation Bool_symKPred := (fun b1 b2 (bR : Bool_R b1 b2)=> Bool_sym _ _ (Bool_sym _ _ bR) = bR).
+Definition Bool_symK : forall b1 b2 (bR : Bool_R b1 b2), Bool_sym _ _ (Bool_sym _ _ bR) = bR.
+Proof.
+refine (fun b1 b2 bR=> Bool_R_ind Bool_symKPred _ _ b1 b2 bR).
+- exact: eq_refl.
+- exact: eq_refl.
+Defined.
+
+Elpi derive.param2 Wrap.
+Elpi derive.sym Wrap.
+
+Notation Wrap_symKPred := (fun w1 w2 (wR : Wrap_R w1 w2) => Wrap_sym _ _ (Wrap_sym _ _ wR) = wR).
+Definition Wrap_symK : forall w1 w2 (wR : Wrap_R w1 w2), Wrap_sym _ _ (Wrap_sym _ _ wR) = wR.
+Proof.
+refine (fun w1 w2 wR=> Wrap_R_ind Wrap_symKPred _ w1 w2 wR).
+refine (fun u1 u2 uR=> _).
+unfold Wrap_sym.
+unfold Wrap_R_rect.
+rewrite Unit_symK.
+exact: eq_refl.
+Defined.
+
+Elpi derive.param2 WrapMore.
+Elpi derive.sym WrapMore.
+
+Notation WrapMore_symKPred := (fun w1 w2 (wR : WrapMore_R w1 w2) => WrapMore_sym _ _ (WrapMore_sym _ _ wR) = wR).
+Definition WrapMore_symK : forall w1 w2 (wR : WrapMore_R w1 w2), WrapMore_sym _ _ (WrapMore_sym _ _ wR) = wR.
+Proof.
+refine (fun w1 w2 wR=> WrapMore_R_ind WrapMore_symKPred _ _ _ w1 w2 wR).
+- refine (fun u1 u2 uR=> _).
+  refine (fun b1 b2 bR=> _).
+  cbn delta beta iota.
+  rewrite Unit_symK Bool_symK.
+  exact: eq_refl.
+- refine (fun w1 w2 wR=> _).
+  cbn delta beta iota.
+  rewrite Wrap_symK.
+  exact: eq_refl.
+- refine (fun u1 u2 uR=> _).
+  refine (fun u3 u4 uR2 => _).
+  refine (fun u5 u6 uR3 => _).
+  cbn delta beta iota.
+  rewrite !Unit_symK. 
+  exact: eq_refl.
+Defined.
+
+Elpi derive.param2 Nat.
+Elpi derive.sym Nat.
+
+Notation Nat_symKPred := (fun w1 w2 (wR : Nat_R w1 w2) => Nat_sym _ _ (Nat_sym _ _ wR) = wR).
+Definition Nat_symK : forall w1 w2 (wR : Nat_R w1 w2), Nat_sym _ _ (Nat_sym _ _ wR) = wR.
+Proof.
+refine (fun w1 w2 wR=> Nat_R_ind Nat_symKPred _ _ w1 w2 wR).
+exact: eq_refl.
+refine (fun n1 n2 nR IH => _).
+cbn delta beta iota.
+rewrite IH.
+exact: eq_refl.
+Defined.
+
+Inductive Box (A : Type) : Type := B : A -> Box A.
+
+Elpi derive.param2 Box.
+Elpi derive.sym Box.
+
+Notation Box_symKPred := (fun A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : Box_R A1 A2 AR b1 b2) => Box_sym A2 A1 (sym_rel AR) b2 b1 (Box_sym A1 A2 AR b1 b2 bR) = bR).
+Definition Box_symK :
+  forall A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : Box_R A1 A2 AR b1 b2), Box_sym A2 A1 (sym_rel AR) b2 b1 (Box_sym A1 A2 AR b1 b2 bR) = bR.
+Proof.
+refine (fun A1 A2 AR=> _).
+refine (fun b1 b2 bR=> _).
+refine (Box_R_ind A1 A2 AR (Box_symKPred A1 A2 AR) _ b1 b2 bR).
+move=> a1 a2 ar.
+cbn delta beta iota.
+exact: eq_refl.
+Defined.
+
+Elpi derive.param2 Option.
+Elpi derive.sym Option.
+
+
+Notation Option_symKPred := (fun A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : Option_R A1 A2 AR b1 b2) => Option_sym A2 A1 (sym_rel AR) b2 b1 (Option_sym A1 A2 AR b1 b2 bR) = bR).
+Definition Option_symK :
+  forall A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : Option_R A1 A2 AR b1 b2), Option_sym A2 A1 (sym_rel AR) b2 b1 (Option_sym A1 A2 AR b1 b2 bR) = bR.
+Proof.
+refine (fun A1 A2 AR=> _).
+refine (fun b1 b2 bR=> _).
+refine (Option_R_ind A1 A2 AR (Option_symKPred A1 A2 AR) _ _ b1 b2 bR).
+- exact: eq_refl.
+- move=> a1 a2 ar.
+cbn delta beta iota.
+exact: eq_refl.
+Defined.
+
+Elpi derive.param2 List.
+Elpi derive.sym List.
+
+Notation List_symKPred := (fun A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : List_R A1 A2 AR b1 b2) => List_sym A2 A1 (sym_rel AR) b2 b1 (List_sym A1 A2 AR b1 b2 bR) = bR).
+Definition List_symK :
+  forall A1 A2 (AR: A1 -> A2 -> Type) b1 b2 (bR : List_R A1 A2 AR b1 b2), List_sym A2 A1 (sym_rel AR) b2 b1 (List_sym A1 A2 AR b1 b2 bR) = bR.
+Proof.
+refine (fun A1 A2 AR=> _).
+refine (fun b1 b2 bR=> _).
+refine (List_R_ind A1 A2 AR (List_symKPred A1 A2 AR) _ _ b1 b2 bR).
+- exact: eq_refl.
+- move=> a1 a2 ar l1 l2 lr IH.
+  cbn delta beta iota.
+  rewrite IH.
+  exact: eq_refl.
+Defined.
+
+Elpi derive.param2 Prod.
+Elpi derive.sym Prod.
+
+Notation Prod_symKPred := (fun A1 A2 (AR: A1 -> A2 -> Type) B1 B2 BR b1 b2 (bR : Prod_R A1 A2 AR B1 B2 BR b1 b2) => Prod_sym A2 A1 (sym_rel AR) B2 B1 (sym_rel BR) b2 b1 (Prod_sym A1 A2 AR B1 B2 BR b1 b2 bR) = bR).
+Definition Prod_symK :
+  forall A1 A2 (AR: A1 -> A2 -> Type) B1 B2 (BR : B1 -> B2 -> Type) b1 b2 (bR : Prod_R A1 A2 AR B1 B2 BR b1 b2), Prod_sym A2 A1 (sym_rel AR) B2 B1 (sym_rel BR) b2 b1 (Prod_sym A1 A2 AR B1 B2 BR b1 b2 bR) = bR.
+Proof.
+refine (fun A1 A2 AR=> _).
+refine (fun B1 B2 BR=> _).
+refine (fun b1 b2 bR=> _).
+refine (Prod_R_ind A1 A2 AR B1 B2 BR (Prod_symKPred A1 A2 AR B1 B2 BR) _ b1 b2 bR).
+- move=> a1 a2 ar b b' br.
+exact: eq_refl.
+Defined.
+
--- std/algo/tests/test_sym.v
+++ std/algo/tests/test_sym.v
@@ -1,30 +1,32 @@
 From elpi Require Import elpi.
 From elpi.apps Require Import derive.param2.
-From Trocq Require Import coverage swap.
+From Trocq Require Import coverage sym.
 
 Elpi derive.param2 False.
-Elpi derive.swap False.
+Elpi derive.sym False.
 
 Elpi derive.param2 Unit.
-Elpi derive.swap Unit.
+Elpi derive.sym Unit.
 
 Elpi derive.param2 Bool.
-Elpi derive.swap Bool.
+Elpi derive.sym Bool.
 
 Elpi derive.param2 Wrap.
-Elpi derive.swap Wrap.
+Elpi derive.sym Wrap.
 
 Elpi derive.param2 WrapMore.
-Elpi derive.swap WrapMore.
+Elpi derive.sym WrapMore.
 
 Elpi derive.param2 nat.
-Elpi derive.swap nat.
+Elpi derive.sym nat.
 
 Elpi derive.param2 option.
-Elpi derive.swap option.
+Elpi Trace Browser.
+Elpi derive.sym option.
+Compute option_sym.
 
 Elpi derive.param2 prod.
-Elpi derive.swap prod.
+Elpi derive.sym prod.
 
 Elpi derive.param2 list.
-Elpi derive.swap list.
+Elpi derive.sym list.
