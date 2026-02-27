42a98a495f41116ff02ab55f4b755866dc4d8329
Tomas Vallejos <tomas.vallejos12@gmail.com>
2026-02-27 14:10:28 +0100

    generating all mapN.Has preservation for inductives and storing them in trocq.db

--- std/algo/tests/test_umap.v
+++ std/algo/tests/test_umap.v
@@ -4,6 +4,10 @@ From Trocq Require Import coverage.
 Unset Uniform Inductive Parameters.
 
 Elpi derive False.
+Check False_map1 : Map1.Has False_R.
+Check False_map2a : Map2a.Has False_R.
+Check False_map2b : Map2b.Has False_R.
+Check False_map3 : Map3.Has False_R.
 Check False_map4 : IsUMap False_R.
 
 Elpi derive Unit.
@@ -34,4 +38,8 @@ Elpi derive ThreeTypes.
 Check ThreeTypes_map4 : forall A1 A2 AR UR B1 B2 BR BUR C1 C2 CR CUR, IsUMap (ThreeTypes_R A1 A2 AR B1 B2 BR C1 C2 CR).
 
 Elpi derive List.
-Check List_map4 : forall A1 A2 AR UR, IsUMap (List_R A1 A2 AR).
+Check List_map1 : forall A1 A2 AR UR, Map1.Has (List_R A1 A2 AR).
+Check List_map2a: forall A1 A2 AR UR, Map2a.Has (List_R A1 A2 AR).
+Check List_map2b: forall A1 A2 AR UR, Map2b.Has (List_R A1 A2 AR).
+Check List_map3: forall A1 A2 AR UR, Map3.Has (List_R A1 A2 AR).
+Check List_map4: forall A1 A2 AR UR, IsUMap (List_R A1 A2 AR).
