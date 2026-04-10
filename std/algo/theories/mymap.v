804c432a77f770ba9e0944a0d202548e2f928662
Enrico Tassi <Enrico.Tassi@Inria.fr>
2026-04-10 16:44:16 +0200

    this change avoid warnings from coqdep

--- std/algo/theories/mymap.v
+++ std/algo/theories/mymap.v
@@ -1,5 +1,5 @@
 From elpi.apps.derive.elpi Extra Dependency "derive_hook.elpi" as derive_hook.
-From Trocq Extra Dependency "algo/elpi/mymap.elpi" as mymap.
+From Trocq.Elpi.X Extra Dependency "mymap.elpi" as mymap.
 
 From elpi Require Import elpi.
 From elpi.apps Require Import derive.legacy.
